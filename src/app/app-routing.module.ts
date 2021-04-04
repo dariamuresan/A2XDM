dbe193fddb4a62948b10e1205dfe6ac409797b0b
Daria Muresan <muresan.daria14@yahoo.com>
2021-04-04 17:12:44 +0300

    Changed routing in navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
   { path : '', redirectTo : '/home', pathMatch: 'full'},
-  { path : 'home', component : HomeComponent}
+  { path : 'home', component : HomeComponent},
+  { path : 'movies', component : MoviesComponent}
 ];
 
 @NgModule({
