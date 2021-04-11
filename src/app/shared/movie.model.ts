b72e425ce228097337b075b469b5574bd401a58c
Daria Muresan <muresan.daria14@yahoo.com>
2021-04-11 22:55:55 +0300

    Added Movie model.

---
+++ src/app/shared/movie.model.ts
@@ -0,0 +1,23 @@
+export interface IMovie {
+    description : string;
+    genres : IGenre[];
+    id : string;
+    rating : number;
+    release_date : string;
+    title : string,
+    actors : IActor,
+    imagePath : string;
+}
+
+export interface IGenre {
+    id : number, 
+    tmdbId : number, 
+    name : string;
+}
+
+export interface IActor {
+    id : number,
+    name : string,
+    imagePath : string,
+    tmdbId : number;
+}
